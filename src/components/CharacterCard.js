d779e3c504dd61387f513e4b8ca743164834a20d
Andre Freeman <asf@mail.usf.edu>
2020-04-07 18:19:31 -0400

    Updated Search Form and Character Card/List; added Styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const CharacterCard = props => {
+  const character = props.character
   return (
   <li>
-      <p>Name: {props.name}</p>
-      <p>Status: {props.status}</p>
-      <p>Species: {props.species}</p>
-      <p>Gender: {props.gender}</p>
+      <p>Name: {character.name}</p>
+      <p>Species: {character.species}</p>
+      <p>Gender: {character.gender}</p>
+      <p>Status: {character.status}</p>
+      <p>Origin: {character.origin.name}</p>
   </li>
   );
 };
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,37 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
-  const [character, setCharacter] = useState([]);
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          console.log(response);
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.log("Server Error", error);
+        });
+    };
+
+    getCharacters();
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-    .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
-    .then(response => {
-      console.log(response);
-      setCharacter(response.data.results)
-    })
-    .catch(err => console.log(err));
   }, []);
 
+  const filtered = characters.filter((c) => c.name.toLowerCase().indexOf(searchText) > -1);
   return (
-    <div>
-      {character.map(arr => {
-        return (
-          <ul>
-            <CharacterCard
-              name={arr.name}
-              status={arr.status}
-              species={arr.species}
-              gender={arr.gender}
-            />
-          </ul>
-        );
+    <section className="character-list">
+      <SearchForm
+        text={searchText}
+        onSearchUpdate={text => setSearchText(text.toLowerCase())}
+      />
+      {filtered.map(character => {
+        return <CharacterCard key={character.id} character={character}></CharacterCard>;
       })}
-    </div>
+    </section>
   );
 }
+
